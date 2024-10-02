b3980d0b8eb18437639d28539f1771efaefb5111
Muhammad Putra Pratama <74628364+renn18@users.noreply.github.com>
2024-10-02 18:07:10 +0800

    Update Posts.jsx

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -34,7 +34,7 @@ const Posts = () => {
             {posts.length > 0 ? <div className="container posts__container">
 
                 {
-                    posts.map(({ id, thumbnail, category, title, description, creator, createdAt }) =>
+                    posts.map(({ _id: id, thumbnail, category, title, description, creator, createdAt }) =>
                         <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} description={description} authorID={creator} createdAt={createdAt} />)
                 }
             </div> : <h2 className='center'>No posts found</h2>}
